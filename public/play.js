f7020f45acb0697c121c2fc0b56b207aa3f22240
Dylan Bailey <dbbailey97@gmail.com>
2023-04-12 10:58:16 -0700

    update play.js

--- public/play.js
+++ public/play.js
@@ -1,3 +1,7 @@
+// Event messages
+const GameEndEvent = 'gameEnd';
+const GameStartEvent = 'gameStart';
+
 const btnDescriptions = 
 [
    { file: 'sound1.mp3', hue: 120}, //green button
@@ -199,6 +203,47 @@ class Game
         }
     
         localStorage.setItem('scores', JSON.stringify(scores));
+    }
+}
+
+// peer communication using WebSocket
+configureWebSocket()
+{
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event) => {
+        this.displayMsg('system', 'game', 'connected');
+    };
+    this.socket.onclose = (event) => {
+        this.displayMsg('system', 'game', 'disconnected');
+    };
+    this.socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+        if (msg.type === GameEndEvent) 
+        {
+            this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+        } 
+        else if (msg.type === GameStartEvent) 
+        {
+            this.displayMsg('player', msg.from, `started a new game`);
+        }
+    };
+
+    displayMsg(cls, from, msg) 
+    {
+        const chatText = document.querySelector('#player-messages');
+        chatText.innerHTML =
+            `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+    }
+    
+    broadcastEvent(from, type, value) 
+    {
+        const event = {
+            from: from,
+            type: type,
+            value: value,
+        };
+        this.socket.send(JSON.stringify(event));
     }
 }
 
