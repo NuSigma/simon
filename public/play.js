e408e3b607d10fefddbc8e75c7e1b33d842dc518
Dylan Bailey <dbbailey97@gmail.com>
2023-04-12 16:03:25 -0700

    configureWebSocket function was outside game class

--- public/play.js
+++ public/play.js
@@ -204,30 +204,31 @@ class Game
     
         localStorage.setItem('scores', JSON.stringify(scores));
     }
-}
 
-// peer communication using WebSocket
-configureWebSocket()
-{
-    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-    this.socket.onopen = (event) => {
-        this.displayMsg('system', 'game', 'connected');
-    };
-    this.socket.onclose = (event) => {
-        this.displayMsg('system', 'game', 'disconnected');
-    };
-    this.socket.onmessage = async (event) => {
-        const msg = JSON.parse(await event.data.text());
-        if (msg.type === GameEndEvent) 
-        {
-            this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-        } 
-        else if (msg.type === GameStartEvent) 
-        {
-            this.displayMsg('player', msg.from, `started a new game`);
-        }
-    };
+
+    // peer communication using WebSocket
+    configureWebSocket()
+    {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+            this.displayMsg('system', 'game', 'connected');
+        };
+        this.socket.onclose = (event) => {
+            this.displayMsg('system', 'game', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+            const msg = JSON.parse(await event.data.text());
+            if (msg.type === GameEndEvent) 
+            {
+                this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+            } 
+            else if (msg.type === GameStartEvent) 
+            {
+                this.displayMsg('player', msg.from, `started a new game`);
+            }
+        };
+    }
 
     displayMsg(cls, from, msg) 
     {
@@ -235,7 +236,7 @@ configureWebSocket()
         chatText.innerHTML =
             `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
     }
-    
+
     broadcastEvent(from, type, value) 
     {
         const event = {
